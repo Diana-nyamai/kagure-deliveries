bfdb639ce37073cf3da75a03d06dbb5f77b80a1b
Diana-nyamai <dnyamai.dn@gmail.com>
2022-06-05 21:18:41 +0300

    update

--- src/components/Map2.js
+++ src/components/Map2.js
@@ -38,88 +38,7 @@ class Map2 extends Component {
    {lat: -1.292622 , lng: 36.779095},
    {lat: -1.291844 , lng: 36.779049}
  ];
- velocity = 100;
- initialDate = new Date();
-
- getDistance = () => {
-   // seconds between when the component loaded and now
-   const differentInTime = (new Date() - this.initialDate) / 1000; // pass to seconds
-   return differentInTime * this.velocity; // d = v*t 
- };
-
- componentDidMount = () => {
-   this.interval = window.setInterval(this.moveObject, 1000);
- };
-
- componentWillUnmount = () => {
-   window.clearInterval(this.interval);
- };
-
- moveObject = () => {
-   const distance = this.getDistance();
-   if (!distance) {
-     return;
-   }
-
-   let progress = this.path.filter(
-     coordinates => coordinates.distance < distance
-   );
-
-   const nextLine = this.path.find(
-     coordinates => coordinates.distance > distance
-   );
-   if (!nextLine) {
-     this.setState({ progress });
-     return; // it's the end!
-   }
-   const lastLine = progress[progress.length - 1];
-
-   const lastLineLatLng = new window.google.maps.LatLng(
-     lastLine.lat,
-     lastLine.lng
-   );
-
-   const nextLineLatLng = new window.google.maps.LatLng(
-     nextLine.lat,
-     nextLine.lng
-   );
-
-   // distance of this line
-   const totalDistance = nextLine.distance - lastLine.distance;
-   const percentage = (distance - lastLine.distance) / totalDistance;
-
-   const position = window.google.maps.geometry.spherical.interpolate(
-     lastLineLatLng,
-     nextLineLatLng,
-     percentage
-   );
-
-   progress = progress.concat(position);
-   this.setState({ progress });
- };
-
- componentWillMount = () => {
-   this.path = this.path.map((coordinates, i, array) => {
-     if (i === 0) {
-       return { ...coordinates, distance: 0 }; // it begins here!
-     }
-     const { lat: lat1, lng: lng1 } = coordinates;
-     const latLong1 = new window.google.maps.LatLng(lat1, lng1);
-
-     const { lat: lat2, lng: lng2 } = array[0];
-     const latLong2 = new window.google.maps.LatLng(lat2, lng2);
-
-     // in meters:
-     const distance = window.google.maps.geometry.spherical.computeDistanceBetween(
-       latLong1,
-       latLong2
-     );
-
-     return { ...coordinates, distance };
-   });
-
-   console.log(this.path);
- };
+ 
   render() {
     return (
         <div>
@@ -138,7 +57,7 @@ class Map2 extends Component {
         >
       
    
-        <Polyline path={this.state.progress} options={{strokeColor: '#ff0000'}} />
+        <Polyline path={this.path} options={{strokeColor: '#ff0000'}} />
        <Marker key="marker_1"
         onClick={this.onMarkerClick}
         title ={'driver A'}
@@ -149,9 +68,7 @@ class Map2 extends Component {
               scaledSize: new google.maps.Size(50, 50) 
               }}
         position = {
-            //  this.path
-            this.state.progress[this.state.progress.length - 1]
-            // {lat: -1.300355, lng: 36.773850}
+            {lat: -1.300355, lng: 36.773850}
                  }
           />
           <InfoWindow 
@@ -161,6 +78,25 @@ class Map2 extends Component {
           <div><h4>{this.state.selectedPlace.name}</h4></div>
           </InfoWindow>
        
+          <Marker key="marker_1"
+        onClick={this.onMarkerClick}
+        title ={'driver B'}
+        name = {'Driver B -1.291879, 36.779049 '}
+        icon={{
+              url: './2.svg',
+              // eslint-disable-next-line no-undef
+              scaledSize: new google.maps.Size(50, 50) 
+              }}
+        position = {
+            {lat:-1.291879, lng:36.778389}
+                 }
+          />
+          <InfoWindow 
+          marker={this.state.activeMarker}
+          visible={this.state.showingInfoWindow}
+          onClose={this.onClose}>
+          <div><h4>{this.state.selectedPlace.name}</h4></div>
+          </InfoWindow>
         
       </Map>
        </div>
