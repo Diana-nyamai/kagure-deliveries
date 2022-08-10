a4f9d55466831a8d5e598161a8dc0f1ae7745c40
Diana-nyamai <dnyamai.dn@gmail.com>
2022-08-10 20:52:11 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 // import Map from './components/Map';
-import Map2 from './components/Map2';
+// import Map2 from './components/Map2';
+import Map3 from './components/Map3';
 
 function App() {
   return (
     <div>
-   <Map2/>
+    <Map3/>
+   {/*<Map2/>*/}
     </div>
   );
 }
--- src/components/Map3.js
+++ src/components/Map3.js
@@ -7,7 +7,7 @@ import {
   Polyline,
 } from "google-maps-react";
 
-class Map2 extends Component {
+class Map3 extends Component {
   state = {
     progress: [],
     showingInfoWindow: false, // Hides or shows the InfoWindow
@@ -46,6 +46,90 @@ class Map2 extends Component {
     { lat: -1.291844, lng: 36.779049 },
   ];
 
+  velocity = 5;
+  initialDate = new Date();
+
+  getDistance = () => {
+    // seconds between when the component loaded and now
+    const differentInTime = (new Date() - this.initialDate) / 1000; // pass to seconds
+    return differentInTime * this.velocity; // d = v*t -- thanks Newton!
+  };
+
+  componentDidMount = () => {
+    this.interval = window.setInterval(this.moveObject, 1000);
+  };
+
+  componentWillUnmount = () => {
+    window.clearInterval(this.interval);
+  };
+
+  moveObject = () => {
+    const distance = this.getDistance();
+    if (!distance) {
+      return;
+    }
+
+    let progress = this.path.filter(
+      coordinates => coordinates.distance < distance
+    );
+
+    const nextLine = this.path.find(
+      coordinates => coordinates.distance > distance
+    );
+    if (!nextLine) {
+      this.setState({ progress });
+      return; // it's the end!
+    }
+    const lastLine = progress[progress.length - 1];
+
+    const lastLineLatLng = new window.google.maps.LatLng(
+      lastLine.lat,
+      lastLine.lng
+    );
+
+    const nextLineLatLng = new window.google.maps.LatLng(
+      nextLine.lat,
+      nextLine.lng
+    );
+
+    // distance of this line
+    const totalDistance = nextLine.distance - lastLine.distance;
+    const percentage = (distance - lastLine.distance) / totalDistance;
+
+    const position = window.google.maps.geometry.spherical.interpolate(
+      lastLineLatLng,
+      nextLineLatLng,
+      percentage
+    );
+
+    progress = progress.concat(position);
+    this.setState({ progress });
+  };
+
+  componentWillMount = () => {
+    this.path = this.path.map((coordinates, i, array) => {
+      if (i === 0) {
+        return { ...coordinates, distance: 0 }; // it begins here!
+      }
+      const { lat: lat1, lng: lng1 } = coordinates;
+      const latLong1 = new window.google.maps.LatLng(lat1, lng1);
+
+      const { lat: lat2, lng: lng2 } = array[0];
+      const latLong2 = new window.google.maps.LatLng(lat2, lng2);
+
+      // in meters:
+      const distance = window.google.maps.geometry.spherical.computeDistanceBetween(
+        latLong1,
+        latLong2
+      );
+
+      return { ...coordinates, distance };
+    });
+
+    console.log(this.path);
+  };
+
+
   render() {
     return (
       <div>
@@ -62,7 +146,9 @@ class Map2 extends Component {
             lng: 36.776811,
           }}
         >
-          <Polyline path={this.path} options={{ strokeColor: "#ff0000" }} />
+        {this.state.progress && (
+          <div>
+          <Polyline path={this.state.progress} options={{ strokeColor: "#ff0000" }} />
           <Marker
             key="marker_1"
             onClick={this.onMarkerClick}
@@ -73,8 +159,11 @@ class Map2 extends Component {
               // eslint-disable-next-line no-undef
               scaledSize: new google.maps.Size(50, 50),
             }}
-            position={{ lat: -1.300355, lng: 36.77385 }}
+            position={ this.state.progress[this.state.progress.length - 1]}
           />
+          </div>
+        )}
+          
           <InfoWindow
             marker={this.state.activeMarker}
             visible={this.state.showingInfoWindow}
@@ -112,6 +201,6 @@ class Map2 extends Component {
   }
 }
 
-export default GoogleApiWrapper({
-  apiKey: "AIzaSyCZ6-yD_cZungcWysRqKsuo7T74CAS1Jsg",
-})(Map2);
+export default  GoogleApiWrapper({
+    apiKey: "AIzaSyCZ6-yD_cZungcWysRqKsuo7T74CAS1Jsg",
+  })(Map3);
