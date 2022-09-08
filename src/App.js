6ddddfd54b4987f05f834d8f5b41251d6533bb6e
Diana-nyamai <dnyamai.dn@gmail.com>
2022-09-08 10:33:31 +0300

    kagure update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import './App.css';
-// import Map from './components/Map';
-// import Map2 from './components/Map2';
-import Map3 from './components/Map3';
+import Map2 from './components/Map2';
+// import Map3 from './components/Map3';
 
 function App() {
   return (
     <div>
-    <Map3/>
-   {/* <Map2/> */}
+    {/* <Map3/> */}
+   <Map2/>
     </div>
   );
 }
--- src/components/Map3.js
+++ src/components/Map3.js
@@ -1,11 +1,5 @@
 import React, { Component } from "react";
-import {
-  Map,
-  GoogleApiWrapper,
-  Marker,
-  InfoWindow,
-  Polyline,
-} from "google-maps-react";
+import {Map,GoogleApiWrapper,Marker,InfoWindow,Polyline} from "google-maps-react";
 
 class Map3 extends Component {
   state = {
@@ -46,7 +40,48 @@ class Map3 extends Component {
     { lat: -1.291844, lng: 36.779049 },
   ];
 
+  velocity = 5
+  initialDate = new Date()
+
+  getDistance = () =>{
+    const differentInTime = (new Date() - this.initialDate) / 1000
+    return differentInTime * this.velocity
+  }
+  consoleDistance = () => {
+    console.log(this.getDistance())
+  }
+  moveObject = () =>{
+    const distance = this.getDistance()
+    if(!distance){
+      return
+    }
+    const progres = this.path.filter(coordinates => coordinates.distance < distance)
+    this.setState({progres})
+  }
   
+  componentDidMount = () =>{
+    this.path = this.path.map((coordinates, i, array) =>{
+      if(i === 0){
+        return {...coordinates, distance: 0} 
+      }
+      const {lat: lat1, lng: lng1} = coordinates
+      const latlang1 = new window.google.maps.LatLng(lat1,lng1)
+
+      const {lat: lat2, lng:lng2} = array[0]
+      const latLong2 = new window.google.map.Latng(lat2, lng2)
+      
+      // distance in meters
+      const distance = window.google.maps.geometry.spherical.computeDistanceVetween(
+        latlang1,
+        latLong2
+      )
+      return {...coordinates, distance}
+    })
+    this.interval = window.setInterval(this.moveObject, 1000)
+  }
+  componentWillUnmount =()=>{
+    window.clearInterval(this.interval)
+  }
 
   render() {
     return (
@@ -58,42 +93,26 @@ class Map3 extends Component {
           zoom={15}
           initialCenter={{lat: -1.298982,lng: 36.776811,}}
         >
-          <Polyline path={this.path} options={{ strokeColor: "#ff0000" }} />
-          <Marker key="marker_1" onClick={this.onMarkerClick} title={"driver A"} name={"Driver A -1.300355, 36.773850 "}
-            icon={{
-              url: "./2.svg",
+          <Polyline path={this.state.progress} options={{ strokeColor: "#ff0000" }} />
+        <Marker key="marker_1" onClick={this.onMarkerClick} title={"driver A"} name={"Driver A -1.300355, 36.773850 "}
+            icon={{url: "./2.svg",
               // eslint-disable-next-line no-undef
-              scaledSize: new google.maps.Size(50, 50),
-            }}
-            position={{ lat: -1.300355, lng: 36.77385 }}
+              scaledSize: new google.maps.Size(50, 50)}}
+            // position={{ lat: -1.300355, lng: 36.77385 }}
+             position ={this.state.progress[0]}
+
           />
-          <InfoWindow
-            marker={this.state.activeMarker}
-            visible={this.state.showingInfoWindow}
-            onClose={this.onClose}
-          >
+          <InfoWindow marker={this.state.activeMarker} visible={this.state.showingInfoWindow} onClose={this.onClose}>
             <div>
               <h4>{this.state.selectedPlace.name}</h4>
             </div>
           </InfoWindow>
 
-          <Marker
-            key="marker_1"
-            onClick={this.onMarkerClick}
-            title={"driver B"}
-            name={"Driver B -1.291879, 36.779049 "}
-            icon={{
-              url: "./2.svg",
-              // eslint-disable-next-line no-undef
-              scaledSize: new google.maps.Size(50, 50),
-            }}
-            position={{ lat: -1.291879, lng: 36.778389 }}
-          />
-          <InfoWindow
-            marker={this.state.activeMarker}
-            visible={this.state.showingInfoWindow}
-            onClose={this.onClose}
-          >
+          <Marker key="marker_1" onClick={this.onMarkerClick} title={"driver B"} name={"Driver B -1.291879, 36.779049 "}icon={{url: "./2.svg",// eslint-disable-next-line no-undef
+             scaledSize: new google.maps.Size(50, 50)}} position={{ lat: -1.291879, lng: 36.778389 }}/>
+
+             {/* info window for marker 2*/}
+          <InfoWindow marker={this.state.activeMarker} visible={this.state.showingInfoWindow} onClose={this.onClose}>
             <div>
               <h4>{this.state.selectedPlace.name}</h4>
             </div>
